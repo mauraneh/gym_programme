3726261ec805a420c99838b144eda4d6c19d25a0
mauraneh <maurane.hugon@gmail.com>
2023-04-14 10:11:52 +0200

    clean

--- public/scripts/graphWeight.js
+++ public/scripts/graphWeight.js
@@ -1,5 +1,3 @@
-
-
 //--------------------- GRAPH POIDS   -------------------------
 
 // SETUP BLOCK
@@ -54,28 +52,3 @@ const graphCanvas = new Chart(
     config
 );
 
-//---------------------GUIDE PROGRAMME -------------------------
-document.querySelector('#exercices').addEventListener('change', function(){
-    let id = document.querySelector('#exercices').selectedOptions[0].value;
-    console.log(id);
-    fetch('index.php?page=api&action=listeRessource&id=' + id)
-        .then(function (response){
-            document.querySelector('#infosExercices').innerHTML = '';
-            response.json().then(function (data){
-
-                let titreVideo = document.createElement('h3');
-                titreVideo.innerText = data[0].nom;
-                document.querySelector('#infosExercices').appendChild(titreVideo);
-
-//TODO: faire un foreach data qui parcours toutes les ressources et en focntion du type de ressource faire un if en
-
-                let video = document.createElement("iframe");
-                // récupérer l'URL de la vidéo à partir de la base de données
-                let urlVideo = data[0].url;
-                // assigner l'URL à la propriété src de l'élément iframe
-                video.setAttribute('src', urlVideo);
-                document.querySelector('#infosExercices').appendChild(video);
-
-            })
-        })
-});
---
+++ public/scripts/guideExercices.js
@@ -0,0 +1,24 @@
+document.querySelector('select').addEventListener('change', function(){
+    let id = document.querySelector('select').selectedOptions[0].value;
+    console.log(id);
+    fetch('index.php?page=api&action=listeRessource&id=' + id)
+        .then(function (response){
+            document.querySelector('#infosExercices').innerHTML = '';
+            response.json().then(function (data){
+
+                let titreVideo = document.createElement('h3');
+                titreVideo.innerText = data[0].nom;
+                document.querySelector('#infosExercices').appendChild(titreVideo);
+
+//TODO: faire un foreach data qui parcours toutes les ressources et en focntion du type de ressource faire un if
+
+                let video = document.createElement("iframe");
+                // récupérer l'URL de la vidéo à partir de la base de données
+                let urlVideo = data[0].url;
+                // assigner l'URL à la propriété src de l'élément iframe
+                video.setAttribute('src', urlVideo);
+                document.querySelector('#infosExercices').appendChild(video);
+
+            })
+        })
+});
